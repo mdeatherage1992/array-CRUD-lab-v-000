11d902080f6740400135e87d436715b4cba838d5
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 18:23:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,8 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
 array = ["Bill","Matt","Mike","Paul"]
-new_array = array.pop
-return new_array
+return array.pop
 end
 
 def remove_element_from_start_of_array(array)
